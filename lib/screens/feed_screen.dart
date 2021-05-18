848ce64aee3e58bc97354776cc5ba4aec4713877
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-05-18 15:32:05 +0530

    modified

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/widgets/posts.dart';
+import 'package:instagram_clone/widgets/stories.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
@@ -17,11 +19,91 @@ class FeedScreen extends StatelessWidget {
             ),
             Image.asset(
               "assets/insta_logo.png",
-              width: 110,
+              width: 120,
             ),
             SvgPicture.asset(
               "assets/messenger.svg",
               width: 24,
+            ),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(right: 20, left: 15, bottom: 10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          width: 65,
+                          height: 65,
+                          child: Stack(
+                            children: <Widget>[
+                              Container(
+                                width: 66,
+                                height: 66,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                    image: AssetImage("assets/jhanvi.JPG"),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                right: 0,
+                                child: Container(
+                                  width: 19,
+                                  height: 19,
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Colors.white),
+                                  child: Icon(
+                                    Icons.add_circle,
+                                    color: Color(0xFF0494F5),
+                                    size: 19,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 9),
+                        Center(
+                          child: Text(
+                            "Your Story",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: List.generate(
+                      5,
+                      (index) => StoryWidget(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              color: Colors.grey.withOpacity(0.1),
+              thickness: 1,
+            ),
+            Column(
+              children: List.generate(
+                5,
+                (index) => PostWidget(),
+              ),
             ),
           ],
         ),
