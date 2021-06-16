07d07a2c43cacaaf7b1cfd4689e7d9c29f4a0aac
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-06-16 13:31:58 +0530

    profile screen changes

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/widgets/add_btmsheet.dart';
+import 'package:instagram_clone/widgets/expand_more_btmsheet.dart';
+import 'package:instagram_clone/widgets/menu_btmsheet.dart';
 
 class ProfileScreen extends StatefulWidget {
   @override
@@ -7,95 +10,50 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  Future _btmSheet2(context) {
+  Future _btmSheet1(context) {
     return showModalBottomSheet(
       context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(17),
+        ),
+      ),
+      // isDismissible: true,
+      // isScrollControlled: true,
       builder: (BuildContext context) {
-        return Wrap(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: 50,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            image: AssetImage("assets/jhanvi.JPG"),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Text("_jhanvi_soni_"),
-                    ],
-                  ),
-                  Container(
-                    width: 32,
-                    height: 32,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.blue,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: [
-                  Container(
-                    width: 50,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1.5,
-                      ),
-                    ),
-                    child: Icon(Icons.add),
-                  ),
-                  SizedBox(width: 10),
-                  Text("Add Account"),
-                ],
-              ),
-            ),
-          ],
-        );
+        return ExapndMoreBtmSheet();
       },
     );
   }
 
-  Future _btmSheet1(context) {
+  Future _btmSheet2(context) {
     return showModalBottomSheet(
       context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(17),
+        ),
+      ),
+      // isDismissible: true,
+      // isScrollControlled: true,
       builder: (BuildContext context) {
-        return Wrap(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                children: [
-                  Text("Settings"),
-                  Text("Archive"),
-                  Text("Your Activity"),
-                  Text("QR Code"),
-                  Text("Saved"),
-                  Text("Close Friends"),
-                  Text("Disover People"),
-                  Text("COVID-19 Information Center"),
-                ],
-              ),
-            ),
-          ],
-        );
+        return AddBtmSheet();
+      },
+    );
+  }
+
+  Future _btmSheet3(context) {
+    return showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(17),
+        ),
+      ),
+      // isDismissible: true,
+      // isScrollControlled: true,
+      builder: (BuildContext context) {
+        return MenuBtmSheet();
       },
     );
   }
@@ -106,7 +64,6 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Builder(
       builder: (context) => Scaffold(
         appBar: AppBar(
-          toolbarHeight: 56,
           elevation: 0,
           title: Row(
             children: [
@@ -114,20 +71,23 @@ class _ProfileScreenState extends State<ProfileScreen> {
               SizedBox(width: 5),
               Text(
                 "_jhanvi_soni_",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w800),
               ),
               GestureDetector(
                 child: Icon(Icons.expand_more),
-                onTap: () => _btmSheet2(context),
+                onTap: () => _btmSheet1(context),
               ),
             ],
           ),
           actions: [
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                "assets/new_post.svg",
-                width: 23,
+              child: GestureDetector(
+                child: SvgPicture.asset(
+                  "assets/new_post.svg",
+                  width: 23,
+                ),
+                onTap: () => _btmSheet2(context),
               ),
             ),
             SizedBox(width: 5),
@@ -135,10 +95,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
               padding: const EdgeInsets.all(12.0),
               child: GestureDetector(
                 child: Icon(
-                  Icons.view_headline_outlined,
+                  Icons.menu,
                   size: 30,
                 ),
-                onTap: () => _btmSheet1(context),
+                onTap: () => _btmSheet3(context),
               ),
             ),
           ],
