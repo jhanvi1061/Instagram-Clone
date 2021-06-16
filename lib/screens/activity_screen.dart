0477d71e447a5f28c2504640c37cc5b1845c56d9
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-06-16 21:11:19 +0530

    added url launcher

--- lib/screens/activity_screen.dart
+++ lib/screens/activity_screen.dart
@@ -1,3 +1,5 @@
+import 'package:url_launcher/url_launcher.dart';
+
 import 'package:flutter/material.dart';
 
 class ActivityScreen extends StatelessWidget {
@@ -5,7 +7,31 @@ class ActivityScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     print("Build() - ActivityScreen");
     return Center(
-      child: Text("Activity Screen"),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'With ♥ by ',
+            style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          GestureDetector(
+            child: Text(
+              'Jhanvi Soni',
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.w500,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+            onTap: () {
+              launch('https://linkedin.com/in/jhanvi1810');
+            },
+          ),
+        ],
+      ),
     );
   }
 }
