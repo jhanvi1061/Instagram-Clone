213b1cf6065d439abe472c81d22b4527dc79698f
jhanvi1061 <jhanvisoni10000@gmail.com>
2021-06-17 13:02:38 +0530

    separated widget for story highlight

---
+++ lib/widgets/story_highlight.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class StoryHighlight extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var widget = Padding(
+      padding: const EdgeInsets.only(bottom: 22.0),
+      child: Container(
+        width: 54,
+        height: 54,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.grey[200],
+        ),
+      ),
+    );
+    return Padding(
+      padding: const EdgeInsets.only(left: 12),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Keep your favorite stories on your profile"),
+          Padding(
+            padding: const EdgeInsets.only(top: 12.0),
+            child: Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: 54,
+                      height: 54,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.grey[300],
+                          width: 1.2,
+                        ),
+                      ),
+                      child: Icon(CupertinoIcons.add, size: 36),
+                    ),
+                    SizedBox(height: 7),
+                    Text(
+                      "New",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
+                SizedBox(width: 26),
+                widget,
+                SizedBox(width: 26),
+                widget,
+                SizedBox(width: 26),
+                widget,
+                SizedBox(width: 26),
+                widget,
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
